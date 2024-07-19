7957ca257fac87fc25ae24875aea5229b6889632
V1daa <mrjokerekoj@gmail.com>
2024-07-19 13:51:59 +0200

    Category update

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -7,12 +7,15 @@ import { getFirebaseStorageUrl } from "@/lib/firebaseUtils";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { LoaderCircle } from "lucide-react";
 import { CardProps } from "@/types/types";
-
+import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider";
 
 export default function Shop() {
   const [loading, setLoading] = useState(true);
   const [images, setImages] = useState<CardProps[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(99999);
 
   const fetchImagesData = async () => {
     const querySnapshot = await getDocs(collection(db, "products"));
@@ -44,13 +47,19 @@ export default function Shop() {
     setLoading(false);
   }, []);
 
-  const filteredImages = selectedCategory
+  let filteredImages = selectedCategory
     ? images.filter((item) => item.category === selectedCategory)
     : images;
 
+  if (minPrice || maxPrice !== 99999) {
+    filteredImages = filteredImages.filter(
+      (item) => item.price <= maxPrice && item.price >= minPrice
+    );
+  }
   return (
     <div className="w-screen h-auto flex justify-center gap-20 p-20 overflow-scroll">
-      <div className="trans w-[300px] h-[400px] sticky top-10 p-5 flex flex-col gap-4">
+      <div className="trans w-[300px] h-auto sticky top-10 p-5 flex flex-col gap-4">
+        <h1>Select a category</h1>
         <div className="flex gap-3 items-center">
           <RadioGroup
             onValueChange={(value) => {
@@ -72,6 +81,37 @@ export default function Shop() {
             </div>
           </RadioGroup>
         </div>
+        <h1>Set minimal price</h1>
+        <input
+          type="number"
+          min={0}
+          name="minValue"
+          id="minValue"
+          max={maxPrice - 1}
+          onChange={(e) => setMinPrice(+e.target.value)}
+          value={minPrice}
+          className="p-2 border-2 border-black border-dotted rounded-xl"
+        />
+        <h1>Set maximal price</h1>
+        <input
+          type="number"
+          name="maxValue"
+          id="maxValue"
+          min={minPrice + 1}
+          onChange={(e) => setMaxPrice(+e.target.value)}
+          value={maxPrice}
+          className="p-2 border-2 border-black border-dotted rounded-xl"
+
+        />
+        <Button
+          onClick={() => {
+            setSelectedCategory("");
+            setMinPrice(0);
+            setMaxPrice(99999);
+          }}
+        >
+          Clear Filters
+        </Button>
       </div>
       <div className="w-full h-full flex flex-wrap gap-10">
         {loading ? (
